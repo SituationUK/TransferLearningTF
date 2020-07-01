a877265369770743c28871570bd49e7ce909c322
Garza Bruno Mateo <BGarza.Deloitte@supervielle.com.ar>
2020-07-01 18:16:02 -0300

    Add project files.

---
+++ TransferLearningTF/ImageData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace TransferLearningTF
+{
+    public class ImageData
+    {
+        [LoadColumn(0)]
+        public string ImagePath;
+
+        [LoadColumn(1)]
+        public string Label;
+    }
+
+    public class ImagePrediction : ImageData
+    {
+        public float[] Score;
+
+        public string PredictedLabelValue;
+    }
+}
